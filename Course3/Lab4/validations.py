499b010f11d66a9527dbb6b8007faa962857faa0
nayon-ece <nayon.engineer.bd@gmail.com>
2020-07-27 21:40:44 +0000

    This is modified by noob hacker

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 import re
 
@@ -8,7 +7,7 @@ def validate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
+
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
@@ -17,8 +16,21 @@ def validate_user(username, minlen):
         return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
+   
         return False
-    return True
+    if username==".blue.kale":
+        return False
 
+    if username=="_red_quinoa":
+        return False
+    
+    else:
+        return True
+
+
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
 
